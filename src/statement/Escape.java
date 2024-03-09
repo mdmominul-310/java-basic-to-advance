da3e943a449e62659f3e7a56be65262920ba24ba
mdmominul-310 <mdmominulislam310@gmail.com>
2024-03-10 01:32:42 +0600

    escape sequence

---
+++ src/statement/Escape.java
@@ -0,0 +1,7 @@
+package statement;
+
+public class Escape {
+    public static void main(String[] args){
+        System.out.print("Hello \\ world");
+    }
+}
---
+++ src/statement/MathFuc.java
@@ -0,0 +1,11 @@
+package statement;
+
+public class MathFuc {
+    public static void main(String[] args){
+//        double random=Math.random()*100;
+//        System.out.print(random);
+//        System.out.print(Math.ceil(2.5));
+//        System.out.print(Math.floor(3.9));
+        System.out.print(Math.sin(90));
+    }
+}

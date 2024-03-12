becd1f66b4e24723d8c7478b7504a78589732cd0
mdmominul-310 <mdmominulislam310@gmail.com>
2024-03-13 00:50:15 +0600

    loops in java

---
+++ src/loop/WhileLoop.java
@@ -0,0 +1,19 @@
+package loop;
+
+public class WhileLoop {
+    public static void main(String[] args){
+//        int i=0;
+//        while(i<=100){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while (i<100);
+for(int i=0;i<=100;i++){
+    System.out.println(i);
+}
+    }
+}

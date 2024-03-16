a961afd0b437fa73d3a25480233813f5f421324f
mdmominul-310 <mdmominulislam310@gmail.com>
2024-03-17 04:31:45 +0600

    multiplication table

--- src/loop/Divisors.java
+++ src/loop/Divisors.java
@@ -1,7 +1,18 @@
 package loop;
 
+import java.util.Scanner;
+
 public class Divisors {
    public static void main(String[] args){
+      // Write a program that takes an integer and prints its divisors, i.e. divisors of 12 are 1, 2, 3, 4, 6.
+       Scanner input=new Scanner(System.in);
+       System.out.print("Enter an integer number: ");
+       int number=input.nextInt();
 
+       for(int i=1;i<=number/2;i++){
+           if(number%i==0){
+               System.out.print(i+" ");
+           }
+       }
    }
 }
---
+++ src/loop/Multiplication.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Multiplication {
+    public static void main(String[] args){
+  // Write a program that prints the multiplication table from 1 to 5
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=10;j++){
+                System.out.print(i*j);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+
+
+    }
+}
